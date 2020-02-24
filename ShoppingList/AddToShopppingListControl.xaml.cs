ea6eb0129c058f3771d5b8b87b640bc56d1c576f
Alex Stueve <amstueve@users.campus>
2020-02-24 15:25:11 -0600

    first commit

---
+++ ShoppingList/AddToShopppingListControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for AddToShopppingListControl.xaml
+    /// </summary>
+    public partial class AddToShopppingListControl : UserControl
+    {
+        public AddToShopppingListControl()
+        {
+            InitializeComponent();
+
+            AddButton.Click += OnAddbuttonClicked;
+        }
+
+        public void OnAddbuttonClicked(object sender, RoutedEventArgs e)
+        {
+            //var data = (ShoppingListData)DataContext;
+            if (DataContext is ShoppingListData data)
+            {
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+
+        }
+    }
+}
